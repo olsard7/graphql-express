77293d85c6df0a22d6160174736accbcaa7b39f8
Oleg <olsard7@gmail.com>
2022-04-18 10:54:24 +0300

    add module.exports

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
-
-const imagesData = [
-  {
-    id: 1,
-    title: "Stacked Brwonies",
-    owner: "Ella Olson",
-    category: "Desserts",
-    url: "https://images.pexels.com/photos/3026804/pexels-photo-3026804.jpeg",
-  },
-  {
-    id: 2,
-    title: "Shallow focus photography of Cafe Latte",
-    owner: "Kevin Menajang",
-    category: "Coffee",
-    url: "https://images.pexels.com/photos/982612/pexels-photo-982612.jpeg",
-  },
-  {
-    id: 3,
-    title: "Sliced Cake on White Saucer",
-    owner: "Quang Nguyen Vinh",
-    category: "Desserts",
-    url: "https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg",
-  },
-  {
-    id: 4,
-    title: "Beverage breakfast brewed coffee caffeine",
-    owner: "Burst",
-    category: "Coffee",
-    url: "https://images.pexels.com/photos/374885/pexels-photo-374885.jpeg",
-  },
-  {
-    id: 5,
-    title: "Pancake with Sliced Strawberry",
-    owner: "Ash",
-    category: "Desserts",
-    url: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
-  },
-];
+const imagesData = require("./imagesData");
 
 const schema = buildSchema(`
     type Query {
