45d790daa83c27ca0aac6cc6cfe19cad64141389
Oleg <olsard7@gmail.com>
2022-04-19 15:25:27 +0300

    add breed images

--- server/dogsData.js
+++ server/dogsData.js
@@ -2,21 +2,28 @@ module.exports = [
   {
     uid: 1,
     breed: "akita",
+    displayImage: "https://images.dog.ceo/breeds/akita/Japaneseakita.jpg",
   },
   {
     uid: 2,
     breed: "boxer",
+    displayImage: "https://images.dog.ceo/breeds/boxer/n02108089_5614.jpg",
   },
   {
     uid: 3,
     breed: "bulldog",
+    displayImage:
+      "https://images.dog.ceo/breeds/bulldog-boston/n02096585_8120.jpg",
   },
   {
     uid: 4,
     breed: "corgi",
+    displayImage:
+      "https://images.dog.ceo/breeds/corgi-cardigan/n02113186_612.jpg",
   },
   {
     uid: 5,
     breed: "doberman",
+    displayImage: "https://images.dog.ceo/breeds/doberman/n02107142_15936.jpg",
   },
 ];
